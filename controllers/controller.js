fa44776df9aea881d61fad97be4361232436bd96
EdMacall <irvedwmac@gmail.com>
2018-10-21 12:33:40 -0400

    fixed controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,6 @@
 var express = require("express");
-var dateFormat = require('dateformat');
 
 var router = express.Router();
-
-// Import the model (burgers.js) to use its database functions.
-var burger  = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -12,7 +8,6 @@ router.get("/", function(req, res) {
     res.render("index");
   });
 });
-
 
 // Export routes for server.js to use.
 module.exports = router;
