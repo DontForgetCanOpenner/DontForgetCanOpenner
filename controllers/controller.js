735d5954500c7a9b3ca34fca4459d0f81b964126
EdMacall <irvedwmac@gmail.com>
2018-10-21 15:09:16 -0400

    added some more routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,6 +8,26 @@ router.get("/about/hurricane", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/about/hurricane.html"));
   });
 
+router.get("/about/wildfires", function(req, res) {
+  console.log("The /about/hurricane route got called.");
+    res.sendFile(path.join(__dirname, "../public/about/wildfires.html"));
+  });
+
+router.get("/about/tornado", function(req, res) {
+  console.log("The /about/hurricane route got called.");
+    res.sendFile(path.join(__dirname, "../public/about/tornado.html"));
+  });
+
+router.get("/about/earthquake", function(req, res) {
+  console.log("The /about/hurricane route got called.");
+    res.sendFile(path.join(__dirname, "../public/about/earthquake.html"));
+  });
+
+router.get("/about/flood", function(req, res) {
+  console.log("The /about/hurricane route got called.");
+    res.sendFile(path.join(__dirname, "../public/about/flood.html"));
+  });
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   console.log("The / route got called.");
